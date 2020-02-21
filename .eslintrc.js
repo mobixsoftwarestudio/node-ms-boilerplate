174273de8c3615abd4e85c43a0403e0e1be2464b
Renato Rodrigues <renato@mobixtec.com>
2020-02-21 16:02:11 -0300

    initial commit with eslint, prettier, nodemon and sucrase

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+const prettierOptions = require('./.prettierrc.js');
+
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'prettier/prettier': ['error', prettierOptions],
+  },
+};
