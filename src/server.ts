0176e4c9b900394e086773f3eb7968863e7043c5
Lucas Harada <lucasharada5@gmail.com>
2020-02-21 16:38:45 -0300

    Adding Helmet to increase security and body-parser to accept JSON

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,11 @@
 import express from 'express';
+import helmet from 'helmet';
+import bodyParser from 'body-parser';
 
 const app = express();
+
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(3434);
