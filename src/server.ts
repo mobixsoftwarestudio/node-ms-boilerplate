ae603468e6026d82735be6dda0b10333baa1a94f
Lucas Harada <lucasharada5@gmail.com>
2020-02-27 14:08:07 -0300

    Create initial structure for Authentication
    Uses local strategy to generate token on login and expects generated token as Bearer Token on protected routes.

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,37 @@
 import express from 'express';
 import helmet from 'helmet';
 import bodyParser from 'body-parser';
+import passport from 'passport';
+import { getToken } from './passport-helper';
 
 const app = express();
 
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(passport.initialize());
+
+app.get('/', (req, res) => res.status(200).json({ status: 'alive' }));
+app.post('/login', (req, res) => {
+  passport.authenticate('local', { session: false }, (err, user, info) => {
+    console.log(err, user, info);
+    if (err) {
+      return res.status(400).json({
+        message: info.message,
+      });
+    }
+    if (!user) {
+      return res.status(403).json({
+        message: info.message,
+      });
+    }
+
+    const token = getToken(user);
+    return res.status(200).json({ token });
+  })(req, res);
+});
+app.get('/protected', passport.authenticate('jwt', { session: false }), (req, res) =>
+  res.status(200).json({ name: 'protected route' }),
+);
 
 app.listen(3434);
